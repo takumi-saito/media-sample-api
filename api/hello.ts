0a9e791216463b7a94506918fa197a3756dcb0ce
takumi-saito <takumi.saitoh.dev@gmail.com>
2024-11-04 05:38:24 +0900

    first commit

---
+++ api/hello.ts
@@ -0,0 +1,7 @@
+// api/hello.ts
+import { NowRequest, NowResponse } from "@vercel/node";
+
+export default function (req: NowRequest, res: NowResponse) {
+  const { name = "World" } = req.query;
+  res.send(`Hello ${name}!`);
+}
