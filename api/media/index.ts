3b6a503e7fd9e0ff0add6450038b0c3b5205ebe2
takumi-saito <takumi.saitoh.dev@gmail.com>
2024-11-05 02:04:16 +0900

    ブラッシュアップ

---
+++ api/media/index.ts
@@ -0,0 +1,46 @@
+import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
+import { handle } from 'hono/nextjs'
+
+export const config = {
+  runtime: 'edge',
+}
+
+// サンプルの楽曲データ
+const songsData = [
+  {
+    "title": "Cheerful_Tunes",
+    "artist": "ISSEI AIR",
+    "duration": 164000,
+    "cover": "https://media-sample-api-tr6g.vercel.app/media/covers/Cheerful_Tunes.jpg",
+    "url": "https://media-sample-api-tr6g.vercel.app/media/tracks/Cheerful_Tunes.mp3"
+  },
+  {
+    "title": "Dream Weaver",
+    "artist": "ISSEI AIR",
+    "duration": 164000,
+    "cover": "https://media-sample-api-tr6g.vercel.app/media/covers/Dream_Weaver.jpg",
+    "url": "https://media-sample-api-tr6g.vercel.app/media/tracks/Dream_Weaver.mp3"
+  }
+];
+
+const app = new Hono().basePath('/api/media')
+
+app.get('/', (c) => {
+  return c.json({
+    message: 'Hello World!',
+  })
+})
+
+app.get(
+  '/media',
+  basicAuth({
+    username: 'mediaapi',
+    password: 'mediaapi-2024',
+  }),
+  (c) => {
+    return c.json(songsData)
+  }
+)
+
+export default handle(app)
