6953339acfcb1e59d8876efaf25038ddfa95e295
takumi-saito <takumi.saitoh.dev@gmail.com>
2024-11-05 00:00:36 +0900

    add code

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,15 @@
 // api/hello.ts
-import { NowRequest, NowResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function (req: NowRequest, res: NowResponse) {
-  const { name = "World" } = req.query;
-  res.send(`Hello ${name}!`);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).json({
+    message: 'Hello world',
+    cookies: req.cookies,
+  })
 }
+// import { NowRequest, NowResponse } from "@vercel/node";
+
+// export default function (req: NowRequest, res: NowResponse) {
+//   const { name = "World" } = req.query;
+//   res.send(`Hello ${name}!`);
+// }
