94816d1b5965290deae90039e831c8a56f9e6ec2
takumi-saito <takumi.saitoh.dev@gmail.com>
2024-11-05 01:54:06 +0900

    fix

--- api/hono/index.ts
+++ api/hono/index.ts
@@ -1,27 +1,17 @@
 import { Hono } from 'hono'
 import { basicAuth } from 'hono/basic-auth'
+import { handle } from 'hono/nextjs'
 
 export const config = {
-    runtime: 'edge',
+  runtime: 'edge',
 }
-const app = new Hono().basePath('/api/hono')
 
-app.use(
-  '/admin/*',
-  basicAuth({
-    username: 'admin',
-    password: 'secret',
-  })
-)
+const app = new Hono().basePath('/api/hono')
 
 app.get('/', (c) => {
   return c.json({
     message: 'Hello from Hono',
   })
-})
-
-app.get('/admin', (c) => {
-  return c.text('You are authorized!')
 })
 
 app.get('/posts/:slug', (c) => {
@@ -32,3 +22,16 @@ app.get('/posts/:slug', (c) => {
     },
   })
 })
+
+app.get(
+  '/auth',
+  basicAuth({
+    username: 'vercel',
+    password: 'hono',
+  }),
+  (c) => {
+    return c.text('Authorized!')
+  }
+)
+
+export default handle(app)
