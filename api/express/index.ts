4b9fe5e7141ecffb429f20eba86683bf6a7e0d94
takumi-saito <takumi.saitoh.dev@gmail.com>
2024-11-05 01:20:56 +0900

    add code

--- api/express/index.ts
+++ api/express/index.ts
@@ -4,6 +4,24 @@ const app = express();
 
 const router = Router();
 
+// サンプルの楽曲データ
+const songsData = [
+  {
+    "title": "Cheerful_Tunes",
+    "artist": "ISSEI AIR",
+    "duration": 164000,
+    "cover": "/media/covers/Cheerful_Tunes.jpg",
+    "url": "/media/tracks/Cheerful_Tunes.mp3"
+  },
+  {
+    "title": "Dream Weaver",
+    "artist": "ISSEI AIR",
+    "duration": 164000,
+    "cover": "/media/covers/Dream_Weaver.jpg",
+    "url": "/media/tracks/Dream_Weaver.mp3"
+  }
+];
+
 router.get('/', (_, res) => {
   res.json({
     message: 'Hello from express!',
@@ -17,6 +35,11 @@ router.get('/posts/:slug', (req, res) => {
       slug: req.params['slug'],
     },
   });
+});
+
+// 新しい楽曲一覧のルート
+router.get('/songs', (_, res) => {
+  res.json(songsData);
 });
 
 app.use('/api/express', router);
